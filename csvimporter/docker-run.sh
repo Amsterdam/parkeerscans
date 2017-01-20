ae9061764e1911d233cf37fc13d5f41b7b7ca72c
Stephan Preeker <stephan@preeker.net>
2017-01-20 12:06:08 +0100

    tg-537 WIP jenkins

---
+++ csvimporter/docker-run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -u   # crash on missing env variables
+set -e   # stop on any error
+
+# prepare elastic templates for scan documents
+# we need to put in manual elk point
+
+
+curl -s -v -f -XDELETE http://${ELKHOST:-elasticsearch}:9200/_template/scan || echo 'OK'
+curl -s -v -f -XPUT http://${ELKHOST:-elasticsearch}:9200/_template/scan -d '
+{
+  "template": "scans-*",
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 0
+  },
+  "mappings": {
+    "scan": {
+      "properties": {
+        "geo": {
+          "type": "geo_point"
+        }
+      }
+    }
+  }
+}'
+
+
+logstash -f readdb.conf --pipeline.workers 8
