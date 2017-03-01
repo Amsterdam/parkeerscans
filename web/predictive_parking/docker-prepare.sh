9330778110597c9bb9522add977cddfeaaed48c7
Stephan Preeker <stephan@preeker.net>
2017-03-01 18:03:04 +0100

    tg-#798 improve import TESTING

--- web/predictive_parking/docker-prepare.sh
+++ web/predictive_parking/docker-prepare.sh
@@ -12,14 +12,16 @@ python get_os_data.py
 
 #[ "$(ls -A /app/unzipped/)" ] && echo "Not Empty" || echo "Empty"
 
+ls -A /app/unzipped/
+
 if [ "$(ls -A /app/unzipped/)" ]
 then
+	echo "csv already present"
+else
 	echo "lets unzip csvs!"
 	# these commands do not overwirte existing files!!
 	# unzip -n '/app/data/*.zip' -d /app/unzipped/
 	ls /app/data/*.rar | xargs -I rarfile unrar -x rarfile /app/unzipped/ || true
-else
-	echo "csv already present"
 fi
 
 ls -alh /app/unzipped
