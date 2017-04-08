534bd9e00cfcf8712178fc58f4be3ca9f760aac6
Stephan Preeker <stephan@preeker.net>
2017-04-08 22:09:04 +0200

    tg-#887 Hello world api fix settings

--- api/predictive_parking/predictive_parking/settings.py
+++ api/predictive_parking/predictive_parking/settings.py
@@ -193,12 +193,7 @@ REST_FRAMEWORK = dict(
     PAGE_SIZE=100,
 
     MAX_PAGINATE_BY=100,
-    DEFAULT_AUTHENTICATION_CLASSES=(
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ),
-    DEFAULT_PAGINATION_CLASS='drf_hal_json.pagination.HalPageNumberPagination',
-    DEFAULT_PARSER_CLASSES=('drf_hal_json.parsers.JsonHalParser',),
+
     DEFAULT_RENDERER_CLASSES=(
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer'
