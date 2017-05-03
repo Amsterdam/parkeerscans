74ee9e4923147262149e8d4c86d22ef6e4d7ed01
Boris Arkenaar <boris@arkenaar.com>
2017-05-03 16:43:19 +0200

    WIP trying te get the map tiles loaded into the map with leaflet proj4 and proj4leaflet libraries

--- angular/src/app/containers/leaflet.directive.ts
+++ angular/src/app/containers/leaflet.directive.ts
@@ -1,12 +1,36 @@
-import { Directive, ElementRef, Input } from '@angular/core';
-import L from 'leaflet';
+import { Directive, ElementRef, Input, NgZone } from '@angular/core';
+//import L from 'leaflet';
+import { MapCrs } from './map-crs.service';
+
+const L = window.L;
+
+const mapOptions = {
+  maxBounds: [
+    [52.269470, 4.72876],
+    [52.4322, 5.07916]
+  ],
+  // 1.0 makes the bounds fully solid, preventing the user from dragging outside the bounds
+  maxBoundsViscosity: 1.0,
+  bounceAtZoomLimits: false,
+  attributionControl: false,
+  zoomControl: false
+}
 
 @Directive({ selector: '[dp-leaflet]' })
 export class LeafletDirective {
-  constructor(el: ElementRef) {
-    const leafletMap: L.Map = L.map(el.nativeElement, {
-      center: [52.3731081, 4.8932945],
-      zoom: 11,
+  constructor(el: ElementRef, zone: NgZone, private crs: MapCrs) {
+    zone.run(() => {
+      const options = Object.assign({}, mapOptions, {
+        crs: this.crs.getRd()
+      });
+      console.log('crs', options);
+      const leafletMap: L.Map = L.map(el.nativeElement, options)
+        .setView([52.3731081, 4.8932945], 11);
+      const baseLayer = L.tileLayer('https://{s}.data.amsterdam.nl/topo_rd/{z}/{x}/{y}.png', {
+        subdomains: ['acc.t1', 'acc.t2', 'acc.t3', 'acc.t4']
+      });
+
+      baseLayer.addTo(leafletMap);
     });
   }
 }
