1d2602c78469130d04031e3a863f20a6c63837a7
Robbin Habermehl <robbin@habermehl.net>
2017-07-04 18:11:59 +0200

    Fix day of the week mapping

--- angular/src/app/containers/statistics/statistics.component.ts
+++ angular/src/app/containers/statistics/statistics.component.ts
@@ -72,7 +72,7 @@ export class StatisticsComponent implements OnInit {
 
           const metingen = res.wegdelen[res.selection.bgt_wegdeel].cardinal_vakken_by_day
               .map((meting) => meting[1].map((_meting) => {
-                _meting[0] = dagenMap[new Date(meting[0]).getDay()];
+                _meting[0] = dagenMap[(new Date(meting[0]).getDay() - 1)];
                 return _meting;
               }))
               .reduce((a, b) => a.concat(b), [])
