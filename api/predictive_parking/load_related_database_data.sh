67d75c0daeb8a759bd59631eba99adf8c520133f
Stephan Preeker <stephan@preeker.net>
2017-11-23 11:55:51 +0100

    fix import tg-#1404

--- api/predictive_parking/load_related_database_data.sh
+++ api/predictive_parking/load_related_database_data.sh
@@ -2,28 +2,35 @@
 
 # helper script to fill database with related data for (development)
 
-set -u
-set -e
+# set -u
+# set -e
 set -x
+echo "provde username if using ssh / dev env"
+
+username=$1
 
 dc() {
 	docker-compose $*;
 }
 
-dc exec database update-table.sh parkeervakken parkeervakken bv predictiveparking
+gettable(){
+	dc exec database update-table.sh $*  $username
+}
+
+gettable parkeervakken parkeervakken bv predictiveparking
 # foutparkeerders / scans niet in vakken
-dc exec database update-table.sh basiskaart BGT_OWGL_verkeerseiland bgt predictiveparking
-dc exec database update-table.sh basiskaart BGT_OWGL_berm bgt predictiveparking
-dc exec database update-table.sh basiskaart BGT_OTRN_open_verharding bgt predictiveparking
-dc exec database update-table.sh basiskaart BGT_OTRN_transitie bgt predictiveparking
-dc exec database update-table.sh basiskaart BGT_WGL_fietspad bgt predictiveparking
-dc exec database update-table.sh basiskaart BGT_WGL_voetgangersgebied bgt predictiveparking
-dc exec database update-table.sh basiskaart BGT_WGL_voetpad bgt predictiveparking
+gettable  basiskaart BGT_OWGL_verkeerseiland bgt predictiveparking
+gettable  basiskaart BGT_OWGL_berm bgt predictiveparking
+gettable  basiskaart BGT_OTRN_open_verharding bgt predictiveparking
+gettable  basiskaart BGT_OTRN_transitie bgt predictiveparking
+gettable  basiskaart BGT_WGL_fietspad bgt predictiveparking
+gettable  basiskaart BGT_WGL_voetgangersgebied bgt predictiveparking
+gettable  basiskaart BGT_WGL_voetpad bgt predictiveparking
 
 # scans op wegen en vakken
-dc exec database update-table.sh basiskaart BGT_WGL_parkeervlak bgt predictiveparking
-dc exec database update-table.sh basiskaart BGT_WGL_rijbaan_lokale_weg bgt predictiveparking
-dc exec database update-table.sh basiskaart BGT_WGL_rijbaan_regionale_weg bgt predictiveparking
+gettable basiskaart BGT_WGL_parkeervlak bgt predictiveparking
+gettable basiskaart BGT_WGL_rijbaan_lokale_weg bgt predictiveparking
+gettable basiskaart BGT_WGL_rijbaan_regionale_weg bgt predictiveparking
 
 echo "Load buurt / buurtcombinatie"
-dc exec database update-table.sh bag bag_buurt public predictiveparking
+gettable bag bag_buurt public predictiveparking
