b7adc4a0343fb96789af37315d0b3093004d2cac
Stephan Preeker <stephan@preeker.net>
2017-11-27 14:10:13 +0100

    tg-#1404 verbeter logging

--- api/predictive_parking/docker-el-backup.sh
+++ api/predictive_parking/docker-el-backup.sh
@@ -4,12 +4,12 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 
 
-curl -s -v -f -XPUT http://elasticsearch:9200/_snapshot/backup -d '
+curl -s --trace-ascii -f -XPUT http://elasticsearch:9200/_snapshot/backup -d '
 {
   "type": "fs",
   "settings": {
       "location": "/tmp/backups" }
 }'
 
-curl -s -v -f -XPUT http://elasticsearch:9200/_snapshot/backup/scans?wait_for_completion=true -d '
+curl -s --trace-ascii -f -XPUT http://elasticsearch:9200/_snapshot/backup/scans?wait_for_completion=true -d '
 { "indices": "scans*" }'
