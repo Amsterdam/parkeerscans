52b32ca830c92b0903ae455ad171d4a7830bad72
Stephan Preeker <stephan@preeker.net>
2017-11-28 16:31:55 +0100

    add headers to curl

--- api/predictive_parking/docker-el-backup.sh
+++ api/predictive_parking/docker-el-backup.sh
@@ -4,12 +4,16 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 
 
-curl -s --trace-ascii -f -XPUT http://elasticsearch:9200/_snapshot/backup -d '
+docurl() {
+	curl -s --trace-ascii -H "Content-Type: application/json" -f -XPUT $*
+}
+
+docurl http://elasticsearch:9200/_snapshot/backup -d '
 {
   "type": "fs",
   "settings": {
       "location": "/tmp/backups" }
 }'
 
-curl -s --trace-ascii -f -XPUT http://elasticsearch:9200/_snapshot/backup/scans?wait_for_completion=true -d '
+docurl http://elasticsearch:9200/_snapshot/backup/scans?wait_for_completion=true -d '
 { "indices": "scans*" }'
