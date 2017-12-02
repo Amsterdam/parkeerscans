6c6a31a143a77f3f8e0d67819d5d544d8be219a0
Stephan Preeker <stephan@preeker.net>
2017-12-02 22:24:11 +0100

    tg-#1404 chmod shizzle

--- .jenkins/import-es.sh
+++ .jenkins/import-es.sh
@@ -29,11 +29,12 @@ dces run --rm logstash
 
 #dces run --rm esbackup chmod -R 777 /tmp/backups
 
-#dces run --rm esbackup rm -rf /tmp/backups/*
+dces run --rm elasticsearch rm -rf /tmp/backups/*
 
 dces run --rm esbackup ./docker-el-backup.sh
 
-#dces run --rm esbackup chmod -R 777 /tmp/backups
+# make sure jenkins has premissions to remove the files
+dces run --rm elasticsearch chmod -R 777 /tmp/backups
 
 dces logs elasticsearch
 
