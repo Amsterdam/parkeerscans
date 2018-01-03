c10121f128baac289c18d8931d9a08ac9c393ba2
Stephan Preeker <stephan@preeker.net>
2018-01-03 11:28:13 +0100

    BAC-749 occupation just 1 request at the time

--- api/predictive_parking/docker-occupancy-bakker.sh
+++ api/predictive_parking/docker-occupancy-bakker.sh
@@ -7,10 +7,13 @@ echo "create needed selections"
 python manage.py scrape_occupancy --selections
 
 echo "download new selections"
-python manage.py scrape_occupancy --wegdelen --part 1 &
-python manage.py scrape_occupancy --wegdelen --part 2 &
-python manage.py scrape_occupancy --wegdelen --part 3 &
-python manage.py scrape_occupancy --wegdelen --part 4
+
+python manage.py scrape_occupancy --wegdelen
+
+#python manage.py scrape_occupancy --wegdelen --part 1 &
+#python manage.py scrape_occupancy --wegdelen --part 2 &
+#python manage.py scrape_occupancy --wegdelen --part 3 &
+#python manage.py scrape_occupancy --wegdelen --part 4
 wait
 
 echo "create database tables"
