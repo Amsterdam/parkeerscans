d53bf01545a9919969dc3ed7e03495afd956c062
Stephan Preeker <stephan@preeker.net>
2018-03-06 11:20:32 +0100

    fix db restore

--- .jenkins/backup-db-scans.sh
+++ .jenkins/backup-db-scans.sh
@@ -8,14 +8,13 @@ echo 0.0.0.0:5432:predictiveparking:predictiveparking:insecure > ~/.pgpass
 
 chmod 600 ~/.pgpass
 
-pg_dump   --clean \
-	  -Fc \
-	  -t wegdelen* \
-	  -t metingen* \
-	  # -t occupancy* \
-	  -t scans* \
-	  -t django_migrations  \
-	  -U predictiveparking \
-	  -h 0.0.0.0 -p 5432 \
-	  -f /backups/database.dump \
-	  predictiveparking
+pg_dump --clean \
+	-Fc \
+	-t wegdelen* \
+	-t metingen* \
+	-t scans* \
+	-t django_migrations  \
+	-U predictiveparking \
+	-h 0.0.0.0 -p 5432 \
+	-f /backups/database.dump \
+	predictiveparking
