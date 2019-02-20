c5e8460018341ae20c0c0bb4aca5f4d17d7c863b
Attumm <mbijman.m.m@gmail.com>
2019-02-20 13:37:27 +0100

    Store ingested filesnames, to skip in the next run
    FIXES #OP-565
    
    Added storing of filenames in the objectstore. Subsequent ingestion
    runs can skip files that are already ingested.

--- api/predictive_parking/get_os_data.py
+++ api/predictive_parking/get_os_data.py
@@ -5,17 +5,17 @@ We use the objectstore to get the latest and greatest of the mks dump
 import re
 import isoweek
 import os
+import json
 import logging
 from pathlib import Path
 
 from swiftclient.client import Connection
+from swiftclient.exceptions import ClientException
 
 from dateutil import parser
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__file__)
-
-assert os.getenv('PARKEERVAKKEN_OBJECTSTORE_PASSWORD')
 
 DATE_RE = re.compile('\d\d\d\d\d\d')
 YEAR_RE = re.compile('\d\d\d\d')
@@ -101,7 +101,7 @@ def full_file_list():
     return rars_sorted_by_time
 
 
-def should_we_download_this(rarname: str, start_month: int, end_month: int) -> bool:
+def should_we_download_this(rarname: str, start_month: int, end_month: int, handled_files: set) -> bool:
     """
     Check if we should download this rar files
 
@@ -125,22 +125,26 @@ def should_we_download_this(rarname: str, start_month: int, end_month: int) -> b
             file_month = int(m[0])
 
     if not file_month:
-        log.debug('date not parsed from file name')
+        log.debug('date not parsed from file name: %s', rarname)
         return False
 
     if start_month:
         if file_month < start_month:
-            log.debug('skiped %s, too old', rarname)
+            log.debug('skipped %s, too old', rarname)
             return False
     if end_month:
         if file_month >= end_month:
-            log.debug('skiped %s, too new', rarname)
+            log.debug('skipped %s, too new', rarname)
             return False
+
+    if rarname in handled_files:
+        log.debug('skipped %s file previously added', rarname)
+        return False
 
     return True
 
 
-def get_latest_rarfiles():
+def get_latest_rarfiles(storage):
     """
     Get latest rarfile
     """
@@ -156,6 +160,8 @@ def get_latest_rarfiles():
     if os.getenv('ENDDATE'):
         # 201708
         end_month = int(os.getenv('ENDDATE'))
+
+    handled_files = storage.load_handled_files()
 
     log.debug('STARTDATE: %s', start_month)
     log.debug('ENDDATE: %s', end_month)
@@ -173,13 +179,13 @@ def get_latest_rarfiles():
             rar_files.append((time, object_meta_data))
 
         # given selection pick rar files.
-        if should_we_download_this(rarname, start_month, end_month):
+        if should_we_download_this(rarname, start_month, end_month, handled_files):
             rar_files.append((time, object_meta_data))
 
     return rar_files
 
 
-def download_files(rar_files):
+def download_files(rar_files, storage):
 
     for time, object_meta_data in rar_files:
 
@@ -200,8 +206,77 @@ def download_files(rar_files):
         with open('{}/{}'.format(DATA_DIR, rarname), 'wb') as outputzip:
             outputzip.write(latest_zip)
 
+    #TODO discuss if there is better place, moment within the ETL process
+    #Files are stored but not yet processed added in the database.
+    cleaned_names = (parse_name(i['name']) for _, i in rar_files)
+    storage.save_handled_files(cleaned_names)
+
+
+def parse_name(full_name):
+    return full_name.split('/')[-1]
+
+
+class StoreObjectStorage:
+    def __init__(self, filename='handled_files', connection=None, container_name=None):
+        self.filename = filename
+        self.container_name = container_name
+        self.connection = connection
+        self.whitelisted = [
+            'datapunt_import_status/test',
+            'datapunt_import_status/acceptance',
+            'datapunt_import_status/production',
+        ]
+
+        assert self.container_name is not None
+
+    def load(self, container_name=None, filename=None):
+        try:
+            return self.connection.get_object(container_name or self.container_name, filename or self.filename)
+        except ClientException:
+            raise
+
+    def save(self, data, container_name=None, filename=None):
+        self.connection.put_object(container_name or self.container_name, filename or self.filename, data)
+
+    def save_handled_files(self, file_names, filename=None):
+        """To save the new set of files, combined them with previous seen files."""
+        assert self.container_name in self.whitelisted
+
+        prev_files = self.load_handled_files(filename=filename, suppress=True)
+        combined_files = prev_files.union(set(file_names))
+        json_files = json.dumps({'files': list(combined_files)})
+        try:
+            self.save(json_files, self.container_name, filename or self.filename)
+        except ClientException:
+            log.warning("Failed to save file")
+
+    def load_handled_files(self, filename=None, suppress=False):
+        assert self.container_name in self.whitelisted
+
+        try:
+            item = self.load(self.container_name, filename or self.filename)
+            return set(json.loads(item[1].decode('utf-8'))['files'])
+        except ClientException:
+            if not suppress:
+                log.warning('Exception during connection with object storage, previously handled files are not skipped')
+            return set()
+        except (json.decoder.JSONDecodeError, ValueError):
+            log.warning(
+                'Exception parsing obj storage response, previously handled files are not skipped')
+            return set()
+
 
 if __name__ == '__main__':
     # get_parkeerkans_db_dumps()
-    rar_files_to_download = get_latest_rarfiles()
-    download_files(rar_files_to_download)
+    assert os.getenv('PARKEERVAKKEN_OBJECTSTORE_PASSWORD')
+    if os.getenv('HANDLED_FILES_ENV') is None:
+        log.warning("HANDLED_FILES_ENV not set, previous loaded files are not skipped")
+
+    storage = StoreObjectStorage(
+        filename='handled_files',
+        container_name= os.getenv('HANDLED_FILES_ENV') or 'datapunt_import_status/test',
+        connection=parkeren_conn
+    )
+    rar_files_to_download = get_latest_rarfiles(storage)
+    download_files(rar_files_to_download, storage)
+
