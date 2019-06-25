b67241206d1e852cc89ada83878c1d3893447b5b
Niels Lensink <niels@we-riot.com>
2019-06-25 16:46:12 +0200

    make sure the unrar does not create folders, but all csv files in the same location

--- api/parkeerscans/docker-prepare-csvdata.sh
+++ api/parkeerscans/docker-prepare-csvdata.sh
@@ -9,7 +9,7 @@ python get_os_data.py
 
 echo 'start extracting..'
 
-ls /app/data/*.rar | xargs -I rarfile unar -s rarfile -o /app/unzipped/ || true
+ls /app/data/*.rar | xargs -I rarfile unar -D rarfile -o /app/unzipped/ || true
 rename -v 's/(.*?)(\d{4})(.*?)/export_scandata_stad_$2/g' /app/data/*.rar
 
 ls -alh /app/unzipped
